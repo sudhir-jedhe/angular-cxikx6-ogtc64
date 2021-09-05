ce2a9a841e3109cdd50ebeb7559c39e00b64ddb4
sudhir-jedhe <jedhesudhir@gmail.com>
2021-09-05 10:16:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-cxikx6-ogtc64
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-cxikx6-ogtc64)
---
+++ main.ts
@@ -0,0 +1,38 @@
+import './polyfills';
+
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DemoMaterialModule} from './material-module';
+
+import {TableBasicExample} from './app/table-basic-example';
+import {VaMatTableComponent} from './app/va-mat-table/va-mat-table.component';
+import {ColumnSorterComponent} from './app/va-mat-table/actions/column-sorter/column-sorter.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    DemoMaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+  ],
+  entryComponents: [TableBasicExample, ColumnSorterComponent],
+  declarations: [TableBasicExample, VaMatTableComponent, ColumnSorterComponent],
+  bootstrap: [TableBasicExample],
+  providers: []
+})
+export class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+
+
+/**  Copyright 2019 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
